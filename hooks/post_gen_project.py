571e889e978ae213de1cac2256b9bcedfcdedf58
Ivan Khomutov <i.khomutov@protonmail.com>
2020-02-09 14:30:11 +0100

    Add options for git and venv

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 
 def remove_travis_file():
@@ -18,6 +19,20 @@ def remove_functional_tests():
     shutil.rmtree(os.path.join("tests", "functional"))
 
 
+def initialize_git():
+    subprocess.call(['git', 'init'])
+
+
+def initialize_venv():
+    try:
+        subprocess.call(['python3.7', '-m', 'venv', 'venv'])
+        subprocess.call(['venv/bin/pip', 'install', '-r', 'requirements/dev.txt'])
+        if "{{ cookiecutter.initialize_git }}".lower() != "n":
+            subprocess.call(['venv/bin/pre-commit', 'install'])
+    except FileNotFoundError:
+        print("ERROR: No executable file found for python 3.7")
+
+
 def main():
 
     if "{{ cookiecutter.ci_tool }}".lower() != "travis":
@@ -29,6 +44,12 @@ def main():
     if "{{ cookiecutter.use_rest_framework }}".lower() != "y":
         remove_drf_files()
         remove_functional_tests()
+
+    if "{{ cookiecutter.initialize_git }}".lower() != "n":
+        initialize_git()
+
+    if "{{ cookiecutter.initialize_venv }}".lower() != "n":
+        initialize_venv()
 
 
 if __name__ == "__main__":
