4e3f4797970661f380335b906dcc474da8cd89ed
Ivan Khomutov <i.khomutov@protonmail.com>
2020-03-05 22:05:56 +0100

    Removed drf-yasg

--- {{cookiecutter.repo_slug}}/{{cookiecutter.project_slug}}/apps/api/urls.py
+++ {{cookiecutter.repo_slug}}/{{cookiecutter.project_slug}}/apps/api/urls.py
@@ -1,23 +1,11 @@
 from django.urls import include
 from django.urls import path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 from .users import urls as users_urls
 
 app_name = 'api'
-schema_view = get_schema_view(
-    openapi.Info(
-        title='{{ cookiecutter.project_name }} API',
-        default_version='v1',
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 
 urlpatterns = [
     path('users/', include(users_urls)),
-    path('', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
--- {{cookiecutter.repo_slug}}/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.repo_slug}}/{{cookiecutter.project_slug}}/settings/base.py
@@ -51,7 +51,6 @@ THIRD_PARTY_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'drf_yasg',
     {%- endif %}
 ]
 LOCAL_APPS = [
@@ -226,20 +225,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-}
-
-# SWAGGER
-# ------------------------------------------------------------------------------
-SWAGGER_SETTINGS = {
-    'USE_SESSION_AUTH': False,
-    'SECURITY_DEFINITIONS': {
-        'Token': {
-            'type': 'apiKey',
-            'in': 'header',
-            'name': 'Authorization'
-        }
-    },
 }
 
 # DJANGO CORS HEADERS
